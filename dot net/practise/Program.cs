752a59807b2e02c23acf9e38435e67b28800a751
AkshayKumar1808 <168962706+AkshayKumar1808@users.noreply.github.com>
2024-10-12 15:44:12 +0530

    abstract class, upcasting, downcasting

---
+++ dot net/practise/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace practise
+{
+    internal class Program
+    {
+        static void CallFun(Animal[] a)
+        {
+            foreach(Animal i in a)
+            {
+
+                i.Eat();
+                Tiger t=i as Tiger;//check the Animal ref to tiger if true "as" reurn the ref rather than return null
+                if (t != null)
+                {
+                    t.Hunting();
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Animal[] arr = new Animal[2];
+            arr[0] = new Dog();
+            arr[1]=new Tiger();
+            CallFun(arr);
+        }
+    }
+}
